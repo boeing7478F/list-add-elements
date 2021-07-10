c8a93fdb22cfd63d799b40a2175c234c70c2ee05
boeing7478F <85016918+boeing7478F@users.noreply.github.com>
2021-07-10 16:50:13 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index2 = 0; index2 < list.length; index2++) {
+        sum = sum + list[index]
+        index += 1
+    }
+    basic.showNumber(sum)
+})
+let list: number[] = []
+let sum = 0
+let index = 0
+index = 0
+sum = 0
+list = [
+3,
+4,
+5,
+7,
+4,
+3,
+2,
+7,
+8,
+9
+]
